60bdb297d5b457902fe5f821c8a594a1d2f6157e
openjny <openjny@gmail.com>
2023-11-17 23:21:54 +0900

    misc

---
+++ constraint.go
@@ -0,0 +1,80 @@
+package main
+
+const (
+	ILLEGAL = "<illegal>"
+	EOF     = "<eof>"
+
+	// https://github.com/Azure/bicep/blob/main/src/Bicep.Core/LanguageConstants.cs
+	KEYWORD_TARGET_SCOPE = "targetScpe"
+	KEYWORD_METADATA     = "metadata"
+	KEYWORD_TYPE         = "type"
+	KEYWORD_USING        = "using"
+	KEYWORD_OUTPUT       = "output"
+	KEYWORD_VAR          = "var"
+	KEYWORD_RESOURCE     = "resource"
+	KEYWORD_MODULE       = "module"
+	KEYWORD_TEST         = "test"
+	KEYWORD_FUNC         = "func"
+	KEYWORD_EXISTING     = "existing"
+	KEYWORD_IMPORT       = "import"
+	KEYWORD_PROVIDER     = "provider"
+	KEYWORD_ASSERT       = "assert"
+	KEYWORD_WITH         = "with"
+	KEYWORD_AS           = "as"
+	KEYWORD_FROM         = "from"
+
+	KEYWORD_IF  = "if"
+	KEYWORD_FOR = "for"
+	KEYWORD_IN  = "in"
+
+	TYPE_ARRAY  = "array"
+	TYPE_OBJECT = "object"
+
+	TARGET_SCOPE_TYPE_TENANT           = "tenant"
+	TARGET_SCOPE_TYPE_MANAGEMENT_GROUP = "managementGroup"
+	TARGET_SCOPE_TYPE_SUBSCRIPTION     = "subscription"
+	TARGET_SCOPE_TYPE_RESOURCE_GROUP   = "resourceGroup"
+
+	LOOP_IDENTIFIER_COPY = "copy"
+
+	KEYWORD_TRUE  = "true"
+	KEYWORD_FALSE = "false"
+	KEYWORD_NULL  = "null"
+	KEYWORD_VOID  = "void"
+
+	FUNCTION_PREFIX_LIST = "list"
+
+	MODULE_PROPERTY_PARAMS  = "params"
+	MODULE_PROPERTY_OUTPUTS = "outputs"
+	MODULE_PROPERTY_NAME    = "name"
+
+	TEST_PROPERTY_PRARMS = "params"
+
+	RESOURCE_PROPERTY_SCOPE      = "scope"
+	RESOURCE_PROPERTY_PARENT     = "parent"
+	RESOURCE_PROPERTY_DEPENDS_ON = "dependsOn"
+	RESOURCE_PROPERTY_LOCATION   = "location"
+	RESOURCE_PROPERTY_PROPERTIES = "properties"
+	RESOURCE_PROPERTY_ASSERTS    = "asserts"
+
+	TYPE_NAME_STRING = "string"
+	TYPE_NAME_BOOL   = "bool"
+	TYPE_NAME_INT    = "int"
+	TYPE_NAME_MODULE = "module"
+	TYPE_NAME_TEST   = "test"
+
+	STRING_DELIMITER  = "'"
+	STRING_HOLE_OPEN  = "${"
+	STRING_HOLE_CLOSE = "}"
+
+	ASSIGN         = "="
+	STRING_LITERAL = "'"
+
+	ARRAY
+	BOOL
+	INT
+	OBJECT
+	SECURE_OBJECT
+	SECURE_STRING
+	STRING
+)
